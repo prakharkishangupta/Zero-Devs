b1e33f9e8ea35c451ee5e6f8041bfaef2ccd5e09
popeyewrener <72228782+popeyewrener@users.noreply.github.com>
2024-09-30 15:12:27 +0530

    Delete server directory

--- server/utils/validatePassword.js
+++
@@ -1,7 +0,0 @@
-const validatePassword = (pass) => {
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_\-+=]).{8,}$/;
-  const isValidPassword = passwordRegex.test(pass);
-  return isValidPassword;
-};
-
-module.exports = validatePassword;
